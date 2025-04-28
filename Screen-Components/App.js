337bf0c5ec268fadd8f7e0706e6c3937466412f2
36465 <36465@rpsk12.org>
2025-04-28 13:38:26 -0400

    Done?

---
+++ Screen-Components/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      {/* Navbar */}
+      <View style={styles.navbar}>
+        <Text>navbar</Text>
+      </View>
+
+      <View style={styles.jumbotronSection}>
+        <Text style={{ color: '#FF6B35' }}>image/video</Text>
+      </View>
+
+      <View style={styles.secondMainSection}>
+        <Text>section 2</Text>
+      </View>
+
+      <View style={styles.footer}>
+        <Text>footer</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    width: '100%',
+  },
+
+  navbar: {
+    height: '10%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ff0000',
+    width: '100%',
+  },
+
+  jumbotronSection: {
+    height: '35%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#0F0',
+    width: '100%',
+    padding: 20,
+  },
+
+  secondMainSection: {
+    height: '35%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00F',
+    width: '100%',
+    padding: 20,
+  },
+
+  footer: {
+    height: '20%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFA73D',
+    width: '100%',
+  },
+});
+
+export default App;
